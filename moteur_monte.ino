4228feb859718ed5fe68786a7d55d91d681ec56b
Chiara20 <60842158+Chiara20@users.noreply.github.com>
2020-03-01 12:16:49 +0100

    Add files via upload

---
+++ moteur_monte.ino
@@ -0,0 +1,67 @@
+// ce code sert à changer la rotation du moteur pour la montée 
+// --- les variables : déclaration et initialisation
+
+int E2 = 6;    //M2 : vitesse du moteur M2
+int M2 = 7;    //M2 Direction : horaire/anti-horaire
+int pinTrig = 2;
+int pinEcho = 3;
+long temps 
+float distance 
+
+// --- le Programme : setup() et loop()
+void setup() { 
+   pinMode(E2, OUTPUT); 
+   pinMode(M2, OUTPUT);  
+   pinMode(pinTrig, OUTPUT);
+   pinMode(pinEcho, INPUT);
+   
+   digitalWrite(pinTrig, LOW);
+   
+   Serial.begin(9600);
+
+} 
+  
+void loop() {
+  digitalWrite(pinTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(pinTrig, LOW);
+
+  temps =  pulseIn(pinEcho, HIGH);
+  if (temps> 25000) {
+    Serial.println("Echec de la mesure");
+  }
+  else{
+    temps = temps/2;
+    distance =(temps*340)/10000.0;
+    Serial.print("Distance: ")
+    Serial.print(distance);
+    Serial.println(" cm");
+    
+  }
+  delay(2000);
+  
+  if (distance < 1) {
+    stopped();
+    delay(100000);
+    \\changement de sens du moteur
+    back_off (155,155)  
+    }
+  else {
+     avance();
+      }
+}
+ 
+// --- Les fonctions -----
+void avance()  { // AVANCE
+  analogWrite (E2,100);    
+  digitalWrite(M2,HIGH);
+}  
+void stopped() {
+  Serial.println("STOP");
+  digitalWrite(E2,0);
+}
+void back_off (char a,char b) {
+  Serial.println("ARRIERE");
+  analogWrite (E2,b);
+  digitalWrite(M2,LOW);
+}
