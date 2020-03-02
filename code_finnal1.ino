c7d0eccce127c2793f107729028304686e5e6a93
Chiara20 <60842158+Chiara20@users.noreply.github.com>
2020-03-02 19:14:57 +0100

    Update code_finnal1.ino

--- code_finnal1.ino
+++ code_finnal1.ino
@@ -1,11 +1,9 @@
 #include <Servo.h>
-//le crochet de la crémaillère toure de 90 degré
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
 // --- les variables : déclaration et initialisation
 
+Servo myservo;  
+int pos = 0;    // variable to store the servo position
 int E2 = 6;    //M2 : vitesse du moteur M2
 int M2 = 7;    //M2 Direction : horaire/anti-horaire
 int pinTrig1 = 2;
@@ -32,22 +30,32 @@ void setup() {
    Serial.begin(9600);
 }
 
+
+
 void loop() {
+ 
+ //le crochet de la crémaillère tourne de 90 degré
   for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-    float distance = ultra_son();
+ 
+ 
+ //le capteur ultrason dectecte le bareau.
+    float distance = ultra_son1();
+ // si la distance est plus petite que 1cm le moteur s'arrete.
   if (distance < 1 ) {
     stopped();
     delay(10000);  
     }
+ //sinon le moteur tourne
   else {
      avance();
       }
-    // put your main code here, to run repeatedly:
-//read the sensor;
+ 
+// code du capteur de fin de course 
+ 
    long sensorReading1 = digitalRead(13);
   //map the sensor range to a range of four option:
   int range = map( sensorReading,0);
@@ -61,12 +69,16 @@ void loop() {
       break;
   }
   delay(1);
-  
+ 
+ //Si le capteur de fin de course détecte la fin de l'échelle le moteur s'arrete le temps de déposer la charge. 
+ //Une fois la charge déposée le moteur tourne dans le sens opposée.
+ 
   if( range == case 0){
     stopped();
     delay(60000);
     back_off(155,155);
   }
+ //la montée 
   float distance = ultra_son2();
   if (distance < 1 ) {
     stopped();
@@ -77,23 +89,26 @@ void loop() {
       }
 
   
+ 
+ 
 // --- Les fonctions -----
-void avance()  { // AVANCE
+ 
+ 
+void avance()  {                       // AVANCE
   analogWrite (E2,100);    
   digitalWrite(M2,HIGH);
 }  
-void stopped() {
+void stopped()  {                     //ARRET
   Serial.println("STOP");
   digitalWrite(E2,0);
 }
-void back_off (char a,char b) {
+void back_off (char a,char b) {              //CHANGEMENT DE SENS
   Serial.println("ARRIERE");
- 
-
   analogWrite (E2,b);
   digitalWrite(M2,LOW);
 }
-float ultra_son1() {digitalWrite(pinTrig1, HIGH);
+float ultra_son1() {
+  digitalWrite(pinTrig1, HIGH);            //fonction qui rencoit la distance captée par le capteur ultrason1
   delayMicroseconds(10);
   digitalWrite(pinTrig1, LOW);
 
@@ -113,7 +128,8 @@ float ultra_son1() {digitalWrite(pinTrig1, HIGH);
   return distance;
   
 }
-float ultra_son2() {digitalWrite(pinTrig2, HIGH);
+float ultra_son2() {                           //fonction qui rencoit la distance captée par le capteur ultrason1
+ digitalWrite(pinTrig2, HIGH);
   delayMicroseconds(10);
   digitalWrite(pinTrig2, LOW);
 
@@ -127,9 +143,39 @@ float ultra_son2() {digitalWrite(pinTrig2, HIGH);
     Serial.print("Distance: ");
     Serial.print(distance);
     Serial.println(" cm");
-    
   }
   delay(2000);
   return distance;
+ 
+ 
+ // j'ai préférée pas touvher au code du haut 
+ // j'ai juste créer 2 fonctions montée et décente quand elles seront complete je pense u'on peut mettre que ça dans void loop :)
+ void decente(){
+  float distance == ultra_son1();
+   // si la distance est plus petite que 1cm le moteur s'arrete.
+  if (distance < 1 ) {
+    stopped();
+    delay(10000);  
+    }
+ //sinon le moteur tourne
+  else {
+     avance();
+      }
   
-}
+  
+ }
+ 
+ void monte(){
+  float distance == ultra_son2();
+   // si la distance est plus petite que 1cm le moteur s'arrete.
+  if (distance < 1 ) {
+    stopped();
+    delay(10000);  
+    }
+ //sinon le moteur tourne
+  else {
+     avance();
+      }
+  
+ }
+  
